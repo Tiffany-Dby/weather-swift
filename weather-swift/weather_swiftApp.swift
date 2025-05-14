c8e74becb32066709575f4b80bc7f1ba6f3e27a2
Tiffany-Dby <tiffany.dby@gmail.com>
2025-05-14 15:39:35 +0200

    init swift project

---
+++ weather-swift/weather_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_swiftApp.swift
+//  weather-swift
+//
+//  Created by Tiffany Dbeissy on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather-swiftTests/weather_swiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  weather_swiftTests.swift
+//  weather-swiftTests
+//
+//  Created by Tiffany Dbeissy on 14/05/2025.
+//
+
+import Testing
+
+struct weather_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
